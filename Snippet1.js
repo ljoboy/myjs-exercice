b88749ce93acd80732605be6752f767cc2366e57
Dark Angel <jonathanyombo@gmail.com>
2019-02-27 11:03:17 +0200

    class Receipt

--- Snippet1.js
+++ Snippet1.js
@@ -3,3 +3,13 @@ class Calculator {
         return a + b;
     }
 }
+
+class Receipt {
+    constructor(calculator) {
+        this.calc = calculator;
+    }
+    print(itemA, itemB) {
+        const total = this.calc.sum(itemA, itemB);
+        console.log(`total receipt £${total}`);
+    }
+}
