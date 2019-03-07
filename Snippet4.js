6f7e1ad73eb8b60ce599a017d36f2258868abdf5
Dark Angel <jonathanyombo@gmail.com>
2019-03-07 14:10:18 +0200

    comments 4

---
+++ Snippet4.js
@@ -0,0 +1,6 @@
+// Functionnal paradigm
+const originalArray = [1, 4, 8, 12];
+for (let i = 0; i < originalArray.length; i++) {
+    originalArray[i] = originalArray[i] + 1;
+}
+console.log(originalArray) //[2, 5, 9, 13]
